95a52e5799d0dcead19d1a529ee2169fb08b20cf
Lep <lepsistemas@gmail.com>
2020-08-16 16:15:49 -0300

    Create Splash Screen

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    private static AudioManager instance;
+
+    public static AudioManager Instance {
+        get {
+            return instance;
+        }
+    }
+
+    public static int START_MENU_MUSIC_INDEX = 0;
+
+    public AudioSource[] music;
+    
+    void Start() {
+        instance = this;
+    }
+
+    public void PlayMusic(int index) {
+        this.music[index].Play();
+    }
+}
--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject newGameButton;
+
+    void Start() {
+        AudioManager.Instance.PlayMusic(AudioManager.START_MENU_MUSIC_INDEX);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(this.newGameButton);
+    }
 
     public void NewGame() {
         SceneManager.LoadScene("Lisbon");
